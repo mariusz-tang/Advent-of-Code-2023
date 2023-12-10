ef67e89731621b92922b0bad17007d8b6d5d468a
TheKuroEver <121342646+TheKuroEver@users.noreply.github.com>
2023-12-10 21:29:22 +0000

    Day 10, part 2

--- Day 10/solution.py
+++ Day 10/solution.py
@@ -39,11 +39,58 @@ class PipeGrid:
         y, x = position
         if y < 0 or y >= len(self._grid) or x < 0 or x >= len(self._grid[y]):
             return PipeGrid._connections["."]
-        print(self._grid[y][x])
         return PipeGrid._connections[self._grid[y][x]]
 
+    def clean(self, path):
+        """Remove all special cells which are not part of the path from the grid"""
+        for y in range(len(self._grid)):
+            row = ""
+            for x in range(len(self._grid[y])):
+                if (y, x) in path:
+                    row += self._grid[y][x]
+                else:
+                    row += "."
+            self._grid[y] = row
+        self._start_character = self._get_start_character()
 
-def get_path_length(grid: PipeGrid, direction):
+    def get_inner_cell_count(self):
+        """Return the number of cells inside the path with a 'ray tracing'-like method"""
+        count = 0
+        for y in range(len(self._grid)):
+            self._grid[y] = self._grid[y].replace("S", self._start_character)
+            inside = False
+            wall_start = None
+            for x in range(len(self._grid[y])):
+                if (char := self._grid[y][x]) == "." and inside:
+                    count += 1
+                elif char in "FL":
+                    wall_start = char
+                elif (
+                    char == "|"
+                    or (char == "J" and wall_start == "F")
+                    or (char == "7" and wall_start == "L")
+                ):
+                    inside = not inside
+        return count
+
+    def _get_start_character(self):
+        up = "D" in self.get_connections_at(self.start + vector([-1, 0]))
+        down = "U" in self.get_connections_at(self.start + vector([1, 0]))
+        right = "L" in self.get_connections_at(self.start + vector([0, 1]))
+        if up:
+            if right:
+                return "L"
+            if down:
+                return "|"
+            return "J"
+        if right:
+            if down:
+                return "F"
+            return "-"
+        return "7"
+
+
+def get_path_length(grid: PipeGrid, direction, path):
     def next_direction() -> str:
         connections = grid.get_connections_at(position)
         if connections == "FINISH":
@@ -56,10 +103,10 @@ def get_path_length(grid: PipeGrid, direction):
 
     position = grid.start
     length = 0
-
     while True:
         position = position + PipeGrid.units[direction]
         direction = next_direction()
+        path.append((position.item(0), position.item(1)))
         length += 1
         if direction is None:
             return None
@@ -71,9 +118,16 @@ if __name__ == "__main__":
     with open("Puzzle input.txt", "r") as data:
         grid = PipeGrid(data.read().splitlines())
 
+    path = []
     distance = None
     for dir_ in "UDLR":
-        if (result := get_path_length(grid, dir_)) is not None:
-            distance = result / 2
+        path = []
+        if (result := get_path_length(grid, dir_, path)) is not None:
+            distance = int(result / 2)
+            break
+
+    grid.clean(path)
+    inner_cells = grid.get_inner_cell_count()
 
     print(f"Part 1: The distance is {distance}!")
+    print(f"Part 2: The number of inner cells is {inner_cells}!")
