421a7b5ba7ef210a38019938b964d9db24d40201
TheKuroEver <121342646+TheKuroEver@users.noreply.github.com>
2023-12-13 00:30:28 +0000

    Finish Day 12

--- Day 12/solution.py
+++ Day 12/solution.py
@@ -6,22 +6,22 @@ class Nonorow:
         self.springs = springs
         self.groups = list(map(int, groups))
         self.index_limits = self._get_index_limits()
-    
+
     def __len__(self):
         return len(self.springs)
-    
+
     def __getitem__(self, index):
         return self.springs[index]
-    
+
     def _get_index_limits(self):
         limits = []
         current_limit = len(self)
         for grp_size in reversed(self.groups):
             limits.append(current_limit - grp_size)
             current_limit -= grp_size + 1
-            
+
         return list(reversed(limits))
-    
+
     def has_room_for_group(self, size, position):
         if len(self) < size + position:
             return False
@@ -35,41 +35,62 @@ class Nonorow:
         return True
 
 
+
+
 def get_total_arrangements(row: Nonorow) -> int:
-    total = 0
-    def check_possible(group_ix, position):
+    cache = dict()
+    def get_total_possibilities(group_ix, position):
+        key = (group_ix, position)
+        if key in cache:
+            return cache[key]
         if not row.has_room_for_group(row.groups[group_ix], position):
-            return
+            return 0
         if group_ix + 1 == len(row.groups):
-            if not "#" in row.springs[position + row.groups[group_ix] + 1:]:
+            if not "#" in row.springs[position + row.groups[group_ix] + 1 :]:
                 # There must not be any springs left after the final group
-                nonlocal total
-                total += 1
-            return
-        
+                return 1
+            return 0
+
         next_starting_position = position + row.groups[group_ix] + 1
         next_limit = row.index_limits[group_ix + 1]
         if next_limit < next_starting_position:
-            return
-        
+            return 0
+
+        total = 0
         for ix in range(next_starting_position, next_limit + 1):
-            check_possible(group_ix + 1, ix)
+            total += get_total_possibilities(group_ix + 1, ix)
             if row[ix] == "#":
                 # We must not skip over any broken springs
-                return
-        return
-    
+                break
+        cache[key] = total
+        return total
+
+    total_arrangements = 0
     for ix in range(0, row.index_limits[0] + 1):
-        check_possible(0, ix)
-    return total
+        total_arrangements += get_total_possibilities(0, ix)
+        if row[ix] == "#":
+            # We must not skip over any broken springs
+            break
+    return total_arrangements
 
 
 if __name__ == "__main__":
-    with open("Puzzleinput.txt", "r") as data:
-        rows = [Nonorow(m[1], m[2].split(",")) for m in re.finditer(r"([#?.]+) ((?:\d+,)*\d)", data.read())]
+    with open("Puzzle input.txt", "r") as data:
+        matches = re.finditer(r"([#?.]+) ((?:\d+,)*\d+)", data.read())
 
-    totals = [get_total_arrangements(row) for row in rows]
-    total_arrangements = sum(totals)
-    for ix, total in enumerate(totals):
-        print(rows[ix].springs, rows[ix].groups, total)
+    rows = []
+    part_two_rows = []
+    for m in matches:
+        springs = m[1]
+        groups = m[2].split(",")
+        rows.append(Nonorow(springs, groups))
+
+        springs = "?".join(([springs] * 5))
+        groups = groups * 5
+        part_two_rows.append(Nonorow(springs, groups))
+
+    total_arrangements = sum(get_total_arrangements(row) for row in rows)
+    part_two_total_arrangements = sum(get_total_arrangements(row) for row in part_two_rows)
+
     print(f"Part 1: The total number of arrangements is {total_arrangements}!")
+    print(f"Part 2: The total number of arrangements is {part_two_total_arrangements}!")
