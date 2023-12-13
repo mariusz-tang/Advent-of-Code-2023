c96579bb2122ad58805c976659d27ae082a5c0eb
TheKuroEver <121342646+TheKuroEver@users.noreply.github.com>
2023-12-13 00:49:56 +0000

    Optimize Day 12 for computation speed

--- Day 12/solution.py
+++ Day 12/solution.py
@@ -4,18 +4,13 @@ import re
 class Nonorow:
     def __init__(self, springs, groups):
         self.springs = springs
+        self.length = len(self.springs)
         self.groups = list(map(int, groups))
         self.index_limits = self._get_index_limits()
 
-    def __len__(self):
-        return len(self.springs)
-
-    def __getitem__(self, index):
-        return self.springs[index]
-
     def _get_index_limits(self):
         limits = []
-        current_limit = len(self)
+        current_limit = self.length
         for grp_size in reversed(self.groups):
             limits.append(current_limit - grp_size)
             current_limit -= grp_size + 1
@@ -23,26 +18,22 @@ class Nonorow:
         return list(reversed(limits))
 
     def has_room_for_group(self, size, position):
-        if len(self) < size + position:
+        if self.length < (end_ix := position + size):
             return False
-        if position > 0 and self[position - 1] == "#":
+        if end_ix != self.length and self.springs[end_ix] == "#":
             return False
-        if (end_ix := position + size) != len(self) and self[end_ix] == "#":
+        if position > 0 and self.springs[position - 1] == "#":
             return False
-        for ix in range(position, size + position):
-            if self[ix] == ".":
+        for ix in range(position, end_ix):
+            if self.springs[ix] == ".":
                 return False
         return True
 
 
-
-
 def get_total_arrangements(row: Nonorow) -> int:
     cache = dict()
+
     def get_total_possibilities(group_ix, position):
-        key = (group_ix, position)
-        if key in cache:
-            return cache[key]
         if not row.has_room_for_group(row.groups[group_ix], position):
             return 0
         if group_ix + 1 == len(row.groups):
@@ -58,17 +49,22 @@ def get_total_arrangements(row: Nonorow) -> int:
 
         total = 0
         for ix in range(next_starting_position, next_limit + 1):
-            total += get_total_possibilities(group_ix + 1, ix)
-            if row[ix] == "#":
+            total += get_total_possibilities_cached(group_ix + 1, ix)
+            if row.springs[ix] == "#":
                 # We must not skip over any broken springs
                 break
-        cache[key] = total
         return total
+
+    def get_total_possibilities_cached(group_ix, position):
+        key = (group_ix, position)
+        if not key in cache:
+            cache[key] = get_total_possibilities(group_ix, position)
+        return cache[key]
 
     total_arrangements = 0
     for ix in range(0, row.index_limits[0] + 1):
-        total_arrangements += get_total_possibilities(0, ix)
-        if row[ix] == "#":
+        total_arrangements += get_total_possibilities_cached(0, ix)
+        if row.springs[ix] == "#":
             # We must not skip over any broken springs
             break
     return total_arrangements
@@ -90,7 +86,9 @@ if __name__ == "__main__":
         part_two_rows.append(Nonorow(springs, groups))
 
     total_arrangements = sum(get_total_arrangements(row) for row in rows)
-    part_two_total_arrangements = sum(get_total_arrangements(row) for row in part_two_rows)
+    part_two_total_arrangements = sum(
+        get_total_arrangements(row) for row in part_two_rows
+    )
 
     print(f"Part 1: The total number of arrangements is {total_arrangements}!")
     print(f"Part 2: The total number of arrangements is {part_two_total_arrangements}!")
