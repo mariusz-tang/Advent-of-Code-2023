c6e80d265dabb5b1e574d8adefdd35aaf0dfafe3
TheKuroEver <121342646+TheKuroEver@users.noreply.github.com>
2023-12-15 11:31:40 +0000

    Day 14 part one

--- Day 14/solution.py
+++ Day 14/solution.py
@@ -1,4 +1,29 @@
+from pathlib import Path
+
+
+# Taken from my solution to Day 13
+def transpose(grid):
+    transposed = [""] * len(grid[0])
+    for row in grid:
+        for ix, char in enumerate(row):
+            transposed[ix] += char
+    return transposed
+
+
 if __name__ == "__main__":
-    with open("Test input.txt", "r") as data:
-        pass  # Parse the data here
-    # Solve the problem here
+    with open(Path(__file__).resolve().parent / "Puzzle input.txt", "r") as data:
+        grid = data.read().splitlines()
+
+    total = 0
+    height = len(grid)
+    grid = transpose(grid)
+    for column in grid:
+        next_load = height
+        for pos, char in enumerate(column):
+            if char == "#":
+                next_load = height - pos - 1
+            elif char == "O":
+                total += next_load
+                next_load -= 1
+
+    print(f"Part 1: The total load is {total}!")
