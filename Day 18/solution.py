9ad9b6fc5ef1a1a9990f94a0c3da60d3c208afb8
TheKuroEver <121342646+TheKuroEver@users.noreply.github.com>
2023-12-18 23:21:38 +0000

    Day 18, part two

--- Day 18/solution.py
+++ Day 18/solution.py
@@ -1,100 +1,76 @@
-"""The strategy here will absolutely not work for part two"""
-
-from dataclasses import dataclass
 from pathlib import Path
 import re
 
 
-@dataclass(frozen=True)
 class Move:
-    x: int
-    y: int
-    direction: str
-    distance: int
+    directions = {"U": (0, -1), "D": (0, 1), "L": (-1, 0), "R": (1, 0)}
 
-    def get_connector(self) -> list:
-        next_coord = [self.x, self.y]
-        dim = 1 if self.direction in "UD" else 0
-        dir = 1 if self.direction in "DR" else -1
-        connector = []
-        for _ in range(1, self.distance):
-            next_coord[dim] += dir
-            connector.append(list(next_coord))
-        return connector
+    def __init__(self, x, y, direction, distance) -> None:
+        self.x = x
+        self.y = y
+        self.direction = direction
+        self.distance = distance
 
-    def get_next_coordinate(self) -> list:
-        next_coord = [self.x, self.y]
-        dim = 1 if self.direction in "UD" else 0
-        dir = 1 if self.direction in "DR" else -1
-        next_coord[dim] += dir * self.distance
-        return next_coord
+    def __repr__(self) -> str:
+        return f"[x={self.x}, y={self.y}, direction={self.direction}, distance={self.distance}]"
+
+    def get_destination_from(self, x, y):
+        direction = Move.directions[self.direction]
+        x += direction[0] * self.distance
+        y += direction[1] * self.distance
+        return (x, y)
 
 
-class Grid:
-    def __init__(self, width: int, height: int) -> None:
-        self._grid = [[" " for _ in range(width)] for _ in range(height)]
-        self.width = width
-        self.height = height
+def get_moves(raw_text: str) -> tuple:
+    moves1 = []
+    moves2 = []
+    x1 = 0
+    x2 = 0
+    y1 = 0
+    y2 = 0
+    for m in re.finditer(r"([UDLR]) (\d+) \(#([a-z0-9]{6})\)", raw_text):
+        moves1.append(move1 := Move(x1, y1, m[1], int(m[2])))
+        x1, y1 = move1.get_destination_from(x1, y1)
+        moves2.append(move2 := Move(x2, y2, "RDLU"[int(m[3][5])], int(m[3][0:5], 16)))
+        x2, y2 = move2.get_destination_from(x2, y2)
 
-    def __getitem__(self, index: int) -> list:
-        return self._grid[index]
+    return (moves1, moves2)
 
-    def __iter__(self):
-        return iter(self._grid)
+
+def get_area(moves):
+    area = 0
+    move_count = len(moves)
+    for ix, move in enumerate(moves):
+        move: Move
+        if not move.direction in "LR":
+            continue
+        width = move.distance - 1
+        height = move.y
+
+        if move.direction == "L":
+            height += 1
+            if moves[ix - 1].direction == "D":
+                width += 1
+            if moves[(ix + 1) % move_count].direction == "U":
+                width += 1
+        else:
+            if moves[ix - 1].direction == "U":
+                width += 1
+            if moves[(ix + 1) % move_count].direction == "D":
+                width += 1
+
+        multiplier = 1 if move.direction == "L" else -1
+        area += multiplier * height * width
+    return area
 
 
 def main():
     with open(Path(__file__).resolve().parent / "Puzzle input.txt", "r") as data:
-        matches = re.finditer(r"([UDLR]) (\d+) \(#([a-z0-9]{6})\)", data.read())
+        moves1, moves2 = get_moves(data.read())
 
-    x = 0
-    y = 0
-    min_x = 0
-    min_y = 0
-    max_x = 0
-    max_y = 0
-    moves = []
-    for m in matches:
-        moves.append(move := Move(x, y, m[1], int(m[2])))
-        x, y = move.get_next_coordinate()
-        min_x = min(min_x, x)
-        min_y = min(min_y, y)
-        max_x = max(max_x, x)
-        max_y = max(max_y, y)
+    print(f"Part 1: The area is {get_area(moves1)}!")
+    print(f"Part 2: The area is {get_area(moves2)}!")
 
-    width = max_x - min_x + 1
-    height = max_y - min_y + 1
-    grid = Grid(width, height)
-
-    for move in moves:
-        move: Move
-        grid[move.y - min_y][move.x - min_x] += move.direction
-        for x, y in move.get_connector():
-            grid[y - min_y][x - min_x] = "#"
-        x, y = move.get_next_coordinate()
-        grid[y - min_y][x - min_x] = move.direction + grid[y - min_y][x - min_x]
-
-    count = 0
-    for row in grid:
-        inside = False
-        last_corner = None
-        for cell in row:
-            if cell != " ":
-                count += 1
-                if cell == "#" and last_corner is None:
-                    inside = not inside
-                elif len(cell) == 3:
-                    if last_corner is None:
-                        last_corner = cell
-                    else:
-                        if not ("D" in (combined := cell + last_corner) and "U" in combined):
-                            inside = not inside
-                        last_corner = None
-                        
-            elif inside:
-                count += 1
-
-    print(f"Part 1: The total lava capacity is {count}!")
 
 if __name__ == "__main__":
     main()
