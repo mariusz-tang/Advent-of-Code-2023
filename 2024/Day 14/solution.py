de53d934c103683cfde4cc1d5c284c4b71f6f8f3
Mariusz Tang <mariusz-tang-dev@outlook.com>
2024-12-02 11:57:18 +0000

    Create template files for 2024

---
+++ 2024/Day 14/solution.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+
+def main():
+    with open(Path(__file__).resolve().parent / "Test input.txt", "r") as data:
+        pass  # Parse the data here
+    # Solve the problem here
+
+
+if __name__ == "__main__":
+    main()
