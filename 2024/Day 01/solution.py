7c347e427f3315123f27ec8b16cc118de82ba472
Mariusz Tang <mariusz-tang-dev@outlook.com>
2024-12-02 12:05:59 +0000

    2024 Day 1

--- 2024/Day 01/solution.py
+++ 2024/Day 01/solution.py
@@ -1,10 +1,30 @@
+from collections import defaultdict
 from pathlib import Path
 
 
 def main():
-    with open(Path(__file__).resolve().parent / "Test input.txt", "r") as data:
-        pass  # Parse the data here
-    # Solve the problem here
+    left = list()
+    right = list()
+    with open(Path(__file__).resolve().parent / "Puzzle input.txt", "r") as data:
+        for line in data.readlines():
+            l, r = line.split("   ")
+            left.append(int(l))
+            right.append(int(r))
+        left.sort()
+        right.sort()
+
+    total = 0
+    for l, r in zip(left, right):
+        total += abs(l - r)
+    print(f"Part one: {total}")
+
+    total = 0
+    right_dict = defaultdict(lambda: 0)
+    for r in right:
+        right_dict[r] += 1
+    for l in left:
+        total += l * right_dict[l]
+    print(f"Part two: {total}")
 
 
 if __name__ == "__main__":
